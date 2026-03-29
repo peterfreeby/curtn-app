fdc2617fcfef382bcc332d493dfe70306f0306f6
Peter Freeby <peterfreeby@gmail.com>
2026-03-29 19:27:18 -0400

    fix: correct Phosphor unicode for folder-simple (E25A) and folder-simple-plus (E25E)

--- packages/app/src/components/icons/Icons.tsx
+++ packages/app/src/components/icons/Icons.tsx
@@ -39,8 +39,8 @@ const ICONS = {
   "copy": "\uE1D0",
   trash: "\uE4AC",
   lightning: "\uE2DE",
-  "folder-simple": "\uE268",
-  "folder-simple-plus": "\uE26C",
+  "folder-simple": "\uE25A",
+  "folder-simple-plus": "\uE25E",
 } as const;
 
 export type IconName = keyof typeof ICONS;
